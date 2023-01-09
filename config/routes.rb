c730865279d0624a7c289c7eaffe8ddd908eb060
Win Feigle <win.feigle@gmail.com>
2023-01-09 10:03:45 -0700

    Complete first 10 specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :users
+  resources :users, only: []
+  resources :recipes, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/me', to: 'users#show'
+
+  post '/signup', to: 'users#create'
 end
