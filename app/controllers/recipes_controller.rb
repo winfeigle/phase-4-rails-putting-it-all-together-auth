3bb6939b47c3d47480449bfc5bd4b574d854e6ed
Win Feigle <win.feigle@gmail.com>
2023-01-10 08:19:44 -0700

    Complete lab

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,18 @@
 class RecipesController < ApplicationController
     def index 
-        render json: Recipe.all, status: :ok
+        render json: Recipe.all, status: :created
     end
+
+    def create
+        recipe = @current_user.recipes.create!(recipe_params)
+        render json: recipe, status: :created
+    end
+
+    private
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete, :user_id)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: []
-  resources :recipes, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :recipes, only: [:index, :create]
 
   get '/me', to: 'users#show'
 
